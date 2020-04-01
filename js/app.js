a7f0070159ae648d7ad5a533e07d9fc7d24d04a4
Abdallah <eng.abdallahobaid@gmail.com>
2020-04-01 03:16:15 +0300

    31/03/2020

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 'use strict'
-var imgArray=[];
+var imgArray=[];   // to avoid duplecate the option on the list
+var page1Arr = []; // array of page 1 opject affter use constructor
+var page2Arr = []; // array of page 2 opject affter use constructor
+var pageName = "page1Arr" //to change the name of the t  when we render the page
 
-$.get('../data/page-1.json').then(data=>{
+//get json for page 1
+$.get('../data/page-1.json').then(data => {
     data.forEach(element => {
        var NewImage = new Image(element); 
-       NewImage.renderImg();
-       NewImage.renderSelct();
+       page1Arr.push(NewImage);
+    });
+    renderItems(page1Arr) // To render the page1 after refresh the page
+});
+
+//get json for page2 
+$.get('../data/page-2.json').then(data=>{
+    data.forEach(element => {
+       var NewImage = new Image(element); 
+       page2Arr.push(NewImage);
     });
 })
+///////////////////////////////////////////////
+
+//constractor function to add a prototype to the opject that we get it from the page.json
 function Image(img){
     this.title = img.title,
     this.description = img.description,
@@ -16,25 +31,63 @@ function Image(img){
     this.horns=img.horns    
 }
 
-Image.prototype.renderImg=function(){
-let imgclone=$('#photo-template').clone();
-imgclone.attr("class",this.key);
-imgclone.find('h2').text(this.title);
-imgclone.find('img').attr("src",this.url);
-imgclone.find('img').attr("alt",this.title);
-imgclone.find('p').text(this.description);
-$('main').append(imgclone);
-
-}
-Image.prototype.renderSelct=function(){
-if(imgArray.includes(this.key) == false){
-$('select').append(`<option value =${this.key}>${this.key}</option>`);
-imgArray.push(this.key);
-}
+Image.prototype.renderImg = function(){     // Attach the html to our js code
+    let imgclone=$('#photo-mustache').html();
+    let html = Mustache.render(imgclone,this); // render the optects"this" on the html 
+    $('main').append(html);               // Append the div to the main 
 }
 
-$('select').change(function(){ 
+Image.prototype.renderSelct = function(){   // to add the options to the select
+    if(imgArray.includes(this.key) == false){ // to check if the keyword is in the imgarray "already in the select" dont add it again
+        $('#Filter').append(`<option value =${this.key}>${this.key}</option>`);
+        imgArray.push(this.key);
+    }
+}
+/////////////////////////////////////////////////
+
+//functions
+function renderItems(arr){
+    $("div").remove(); // to remove the img div
+    $('#Filter').children().remove().end(); // to remove all option of the filter selector
+    imgArray=[]; // array for not repeating the filter selecotor options
+    $('#Filter').append(`<option value>Select Keyword</option>`); // To add the "select keyword" each time we change the page or add change the sort
+    arr.forEach(element => {
+        element.renderImg();
+        element.renderSelct();
+    })
+}
+/////////////////////////////////////////////////
+
+//events:P
+$('#Filter').change(function(){ 
     var value = $(this).val();
-    $("section").hide();
-    $(`.${value}`).show();
+    $("div").hide();  // hide all divs
+    $(`.${value}`).show(); // show the div that have a class of value that been selected  
+});
+
+$('#page1').click(function(){ 
+    pageName = "page1Arr"
+    renderItems(page1Arr)
+});
+
+$('#page2').click(function(){ 
+    pageName = "page2Arr"
+    renderItems(page2Arr)
+});
+
+$('#Sort').change(function(){ 
+    var value = $(this).val();
+    var arr = page1Arr
+    if (pageName == "page2Arr"){ // to change the array by the name
+        arr = page2Arr;
+    }
+    arr.sort((a,b) =>{
+        if(b[value] > a[value]){
+          return -1 
+        }
+        else{
+          return b[value] < a[value]
+        }
+      });
+      renderItems(arr);
 });
